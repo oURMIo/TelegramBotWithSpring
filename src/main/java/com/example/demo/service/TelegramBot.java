d736c28badea6f2bad61ebc252000e21394c77d4
oURMIo <dimach.98.ru@gmail.com>
2022-11-21 20:34:45 +0300

    add get request

--- src/main/java/com/example/demo/service/TelegramBot.java
+++ src/main/java/com/example/demo/service/TelegramBot.java
@@ -7,9 +7,17 @@ import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+
+import java.net.URL;
+
 @Component
 public class TelegramBot extends TelegramLongPollingBot {
     final BotConfig config;
+    private int active = 0;
 
     public TelegramBot(BotConfig config) {
         this.config = config;
@@ -33,14 +41,77 @@ public class TelegramBot extends TelegramLongPollingBot {
 
             switch (messageGet) {
                 case "/start" -> start(chatId, update.getMessage().getChat().getFirstName());
-                default -> sendMessage(chatId, "I do not understand you");
+                case "/show" -> {
+                    try {
+                        showUsers(chatId);
+                    } catch (IOException e) {
+                        sendMessage(chatId, "now 'show' don't work");
+                    }
+                }
+                case "/add" -> {
+                    sendMessage(chatId, "Pls, enter the name you want to add to the database - ");
+                    active = 1;
+                }
+                default -> checkActive(chatId, messageGet);
             }
         }
     }
 
+    private void checkActive(Long chatId, String messageGet) {
+        switch (active) {
+            case 1 -> {
+                try {
+                    addUser(chatId, messageGet);
+                } catch (IOException e) {
+                    sendMessage(chatId,"Pls, write username in one word");
+                }
+            }
+            case 2 -> {
+            }
+            default -> sendMessage(chatId, "I do not understand you");
+        }
+    }
+
     private void start(Long chatId, String name) {
-        String messageSend = "Hi honey " + name + " (〃￣︶￣)人(￣︶￣〃)";
+        String messageSend = "Hi honey, " + name + " (〃￣︶￣)人(￣︶￣〃)";
         sendMessage(chatId, messageSend);
+    }
+
+    private void showUsers(Long chatId) throws IOException {
+        String url = "http://chserv.ddns.net:8080/show";
+        URL obj = new URL(url);
+        HttpURLConnection connection = (HttpURLConnection) obj.openConnection();
+
+        connection.setRequestMethod("GET");
+
+        BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+        String inputLine;
+        StringBuilder response = new StringBuilder();
+
+        while ((inputLine = in.readLine()) != null) {
+            response.append(inputLine);
+        }
+        in.close();
+        String messageSend = response.toString();
+        sendMessage(chatId, messageSend);
+    }
+
+    private void addUser(Long chatId, String name) throws IOException {
+        String url = "http://chserv.ddns.net:8080/create/" + name;
+        URL obj = new URL(url);
+        HttpURLConnection connection = (HttpURLConnection) obj.openConnection();
+
+        connection.setRequestMethod("GET");
+
+        BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+        String inputLine;
+        StringBuilder response = new StringBuilder();
+
+        while ((inputLine = in.readLine()) != null) {
+            response.append(inputLine);
+        }
+        in.close();
+        sendMessage(chatId, response.toString());
     }
 
     private void sendMessage(Long chatId, String textMessage) {
